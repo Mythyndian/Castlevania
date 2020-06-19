553b97f41a963056d91b5cc7c4b3be47ba6befaf
Mythyndian <zawistowskimaciej1@gmail.com>
2020-06-19 02:19:13 +0200

    Removed some trash

--- game.py
+++ game.py
@@ -1,15 +1,21 @@
 import pygame
+
 pygame.init()
 WIDTH, HEIGHT = 800, 600
 SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
 run = True
+intro = True
 while run:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            raise SystemExit
-        # KEY DOWN EVENTS
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
+    while intro:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
                 raise SystemExit
 
 
+        # KEY DOWN EVENTS
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    raise SystemExit
+
+pygame.quit()
