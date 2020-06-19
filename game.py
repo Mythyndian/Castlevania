2fe6750aedb6086a22bdfd8897659587427430c1
Mythyndian <zawistowskimaciej1@gmail.com>
2020-06-19 03:49:45 +0200

    Made a start screen

--- game.py
+++ game.py
@@ -1,7 +1,9 @@
 import pygame
+from loadings import FONT
+from loadings import BACKGROUND
 
 pygame.init()
-WIDTH, HEIGHT = 800, 600
+WIDTH, HEIGHT = 1109, 600
 SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
 
 run = True
@@ -10,13 +12,19 @@ while run:
     while intro:
         # Render intro screen
         SCREEN.fill((0, 0, 0))
+        textSurfaceObj = FONT.render('PRESS   ENTER   TO   START   PLAYING', True, (240, 240, 240), (0, 0, 0))
+        textRectObj = textSurfaceObj.get_rect()
+        textRectObj.center = (650, 500)
+        SCREEN.blit(BACKGROUND, (0, 0))
+        SCREEN.blit(textSurfaceObj, textRectObj)
+        pygame.display.update()
         for event in pygame.event.get():
             # When user click exit button
             if event.type == pygame.QUIT:
                 raise SystemExit
             # When user press Enter game start working
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ENTER:
+                if event.key == pygame.K_RETURN:
                     intro = False
 
         # KEY DOWN EVENTS
---
+++ loadings.py
@@ -0,0 +1,5 @@
+import pygame
+
+pygame.init()
+BACKGROUND = pygame.image.load('png/Castlevania_logo.png')
+FONT = pygame.font.Font('font/pixelated.ttf', 34)
