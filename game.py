7ae2f12657ce172a12111c268c28bf8e7e4686e6
Mythyndian <zawistowskimaciej1@gmail.com>
2020-06-19 04:47:59 +0200

    Failed to animate text

--- game.py
+++ game.py
@@ -5,19 +5,21 @@ from loadings import BACKGROUND
 pygame.init()
 WIDTH, HEIGHT = 1109, 600
 SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
-
+CLOCK = pygame.time.Clock()
 run = True
 intro = True
 while run:
     while intro:
         # Render intro screen
-        SCREEN.fill((0, 0, 0))
-        textSurfaceObj = FONT.render('PRESS   ENTER   TO   START   PLAYING', True, (240, 240, 240), (0, 0, 0))
-        textRectObj = textSurfaceObj.get_rect()
-        textRectObj.center = (650, 500)
+        on_text_surface = FONT.render('PRESS   ENTER   TO   START   PLAYING', True, (240, 240, 240), (0, 0, 0))
+        textRectObj = on_text_surface.get_rect()
+        off_text_surface = pygame.Surface(textRectObj.size)
+        textRectObj.center = (625, 500)
         SCREEN.blit(BACKGROUND, (0, 0))
-        SCREEN.blit(textSurfaceObj, textRectObj)
+        SCREEN.blit(on_text_surface, textRectObj)
         pygame.display.update()
+        on_text_surface.fill((0, 0, 0))
+
         for event in pygame.event.get():
             # When user click exit button
             if event.type == pygame.QUIT:
@@ -32,5 +34,5 @@ while run:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     raise SystemExit
-
+    CLOCK.tick(60)
 pygame.quit()
