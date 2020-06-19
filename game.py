ef593c55302776587001f874f1ae955a4916c9dc
Mythyndian <zawistowskimaciej1@gmail.com>
2020-06-19 03:06:56 +0200

    Added cool font

--- game.py
+++ game.py
@@ -3,14 +3,21 @@ import pygame
 pygame.init()
 WIDTH, HEIGHT = 800, 600
 SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
+
 run = True
 intro = True
 while run:
     while intro:
+        # Render intro screen
+        SCREEN.fill((0, 0, 0))
         for event in pygame.event.get():
+            # When user click exit button
             if event.type == pygame.QUIT:
                 raise SystemExit
-
+            # When user press Enter game start working
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ENTER:
+                    intro = False
 
         # KEY DOWN EVENTS
         for event in pygame.event.get():
